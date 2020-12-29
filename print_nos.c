f02c1f2803b305d566e8da3c390e861db1cd5949
ankit-kumar-singh517 <68545337+ankit-kumar-singh517@users.noreply.github.com>
2020-12-29 20:39:54 +0530

    Add files via upload

---
+++ print_nos.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+ int main(int argc, char const *argv[])
+ {
+      int i,n;
+      printf("enter the value of n\n");
+      scanf("%d",&n); /* code */
+      for(i=0;i<=n;i++)
+      {
+          printf("\n%d",i);
+      }
+
+     return 0;
+ }
+ 
