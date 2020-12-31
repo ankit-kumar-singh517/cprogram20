3fb76ab0c7b770531a930545e150ab175c621d53
ankit-kumar-singh517 <68545337+ankit-kumar-singh517@users.noreply.github.com>
2020-12-31 10:00:40 +0530

    Add files via upload

---
+++ if_else_condition.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main(int argc, char const *argv[])
+{
+    int x=10;
+    int y=20;
+    printf("enter the value of the number =");
+    if(x==10)
+    {
+        printf("\n x is correct ");
+;    }
+    else
+    {
+      printf("\n y is correct");
+    }
+    return 0;
+}
