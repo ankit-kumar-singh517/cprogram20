9c6d5ab637cb1b07be792e38630cd4c987a06560
ankit-kumar-singh517 <68545337+ankit-kumar-singh517@users.noreply.github.com>
2021-01-01 20:37:39 +0530

    Add files via upload

---
+++ operator.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{   int z1,z2,z3;
+    int x=12;
+    int y=25;
+    int z;
+    z1=x&y;
+    z2=x++;
+
+  
+    printf("\nvalue of z1=%d",z1);
+    printf("\nvalue of z2=%d",z2);
+    printf("\nvalue of z3=%d",z3);
+    (x>y)?printf("x is greater"):
+    printf("y is greater");
+
+
+    return 0;
+
+}
