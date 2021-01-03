5f0ee1c26d135a10ba984e0b7c963fbce74d1deb
ankit-kumar-singh517 <68545337+ankit-kumar-singh517@users.noreply.github.com>
2021-01-03 21:10:10 +0530

    Add files via upload

---
+++ first.c
@@ -0,0 +1,12 @@
+#include<stdio.h>  //preprocessor directive 
+int main()           //if we use "void" than we dont use return 0;
+{
+   //input->process(logic)->output  //priviple of computer
+   int num=10;
+   printf("hello i m using c commamnd ");
+   scanf("%d",&num);
+   printf("your no.=%d",num);
+   return 0;
+}
+//function is block of code ,which have name and can be execute again and again,
+//it may be possible to pass argument to the function and it can return value if required
