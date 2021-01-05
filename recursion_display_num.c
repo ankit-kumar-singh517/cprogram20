ccb79afb4220144e7f0ab9202fab6b39263f605f
ankit-kumar-singh517 <68545337+ankit-kumar-singh517@users.noreply.github.com>
2021-01-05 09:16:11 +0530

    Add files via upload

---
+++ recursion_display_num.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int displaynum(int);
+int main(int argc, char const *argv[])
+{
+  int n;
+  printf("enter the value of n=>");
+  scanf("%d",&n);
+  int r=displaynum(n);
+
+  return 0;
+}
+int displaynum(int n)
+{
+    if(n<1)
+    return 1;
+    printf("\nvalue of n=>%d",n);
+    displaynum(n-1);
+}
