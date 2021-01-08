08772ca7bc3279aba5b1537ca3f08429ae6013d7
ankit-kumar-singh517 <68545337+ankit-kumar-singh517@users.noreply.github.com>
2021-01-08 10:38:15 +0530

    Add files via upload

---
+++ for_loop_1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main(int argc, char const *argv[])
+{
+    int i,n;
+    printf("enter the value of number=>");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        printf("\n%d",i);
+    }
+    return 0;
+}
