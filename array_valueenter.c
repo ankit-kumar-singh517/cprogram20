5139cedc74dcbeec0d1dff645fe46ca269430781
ankit-kumar-singh517 <68545337+ankit-kumar-singh517@users.noreply.github.com>
2021-01-30 09:32:54 +0530

    Add files via upload

---
+++ array_valueenter.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int i,j;
+    int num3[6];
+    printf("enter 6 array in the array list=>\n");
+    for(i=0;i<=5;i++)
+ {
+     
+     scanf(" %d",&num3[i]);
+   
+
+ }
+    for(j=0;j<=5;j++)
+    {
+        printf("  %d",num3[j]);
+    }
+
+    return 0;
+}
